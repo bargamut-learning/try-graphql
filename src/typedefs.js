3ea1ee8e5a781682f359375df1e676e1e3eaa248
Paul Petrov <paul@bargamut.ru>
2020-06-22 19:39:20 +0300

    Create TypeDefs module for GraphQL

---
+++ src/typedefs.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const typeDefs = gql`
+  type User {
+    id: Int
+    fname: String
+    age: Int
+    likes: Int
+    posts: [Post]
+  }
+
+  type Post {
+    id: Int
+    user: User
+    body: String
+  }
+
+  type Query {
+    users(id: Int!): User!
+    posts(id: Int!): Post!
+  }
+  type Mutation {
+    incrementLike(fname: String!) : [User!]
+  }
+
+  type Subscription {
+    listenLikes : [User]
+  }
+`;
+
+module.exports = {
+	typeDefs,
+};
